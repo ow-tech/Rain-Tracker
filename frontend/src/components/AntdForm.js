3ab33fa18ee37a55cf831ef0a2b1edf6370c47d0
ow-tech <wagwanwekon@outlook.com>
2021-10-22 16:25:39 +0300

    fixed a bug; successfully implemented frontend to connect with posting endpoint. preparation for fixing more bugs

---
+++ frontend/src/components/AntdForm.js
@@ -0,0 +1,140 @@
+import React,{ useEffect, useState} from 'react'
+import { Form, Input, Button, Select} from 'antd';
+
+const AntdForm = (params) => {
+
+  const [formValues, setFormValues] = useState()
+  // const [week, setWeek] = useState()
+  // const [day, setDay] = useState('')
+  // const [rainData, setRainData] = useState()
+  const [error,setError] = useState(null)
+  
+
+  const onFinish = (values) => {
+    // console.log('Success:', values);
+    setFormValues(values)
+    params.upStreamValues(values)
+    postBackend(values)
+  };
+  console.log(formValues)
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  const { Option } = Select;
+
+  // post data
+
+  const postBackend = (formValues)=>{
+    if(formValues){
+      console.log('I am formdata in datalist')
+      console.log(formValues.week)
+      let week = formValues.week;
+      let day = formValues.day;
+      let rainData = formValues.rainData;
+      // setDay(formValues.day)
+      // setRainData(formValues.rainData)
+      console.log('am extratctecd'+ formValues.week)
+      fetch('http://127.0.0.1:8000/api/post/', {
+          method:'post',
+          cache: 'no-cache',
+          headers: {
+              'Content-Type': 'application/json'
+      },
+    
+
+        body: JSON.stringify({
+          week: week,
+          day: day,
+          rainData: rainData
+        })
+    }).then(function(res){
+        return res.json();
+    })
+    .catch(error =>{
+        setError(error)
+    });
+
+    }
+    
+  
+}
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 12,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      // initialValues={{
+      //   remember: true,
+      // }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Week e.g 4"
+        name="week"
+        rules={[
+          {
+            required: true,
+            message: 'Please input week No.',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Day"
+        name="day"
+        rules={[
+          {
+            required: true,
+            message: 'Please Select A day',
+          },
+        ]}
+      >
+        <Select style={{ width: 70 }}>
+        <Option value="1">Sun</Option>
+        <Option value="2">Mon</Option>
+        <Option value="3">Tue</Option>
+        <Option value="4">Wed</Option>
+        <Option value="5">Thur</Option>
+        <Option value="6">Fri</Option>
+        <Option value="7">Sat</Option>
+      </Select>
+      </Form.Item>
+
+      <Form.Item
+        name="rainData"
+        label="Rain Measurement in mm"
+        rules={[
+          {
+            required: true,
+            message: 'Please Input amount',
+          },
+        ]}
+      >
+        <Input/>
+       
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default AntdForm;
--- frontend/src/components/DataList.js
+++ frontend/src/components/DataList.js
@@ -1,7 +1,13 @@
 import React,{ useEffect, useState} from 'react'
-
 import { Layout, Menu, Breadcrumb } from 'antd';
 import Chart from './Chart';
+import AntdForm from './AntdForm';
+
+// modal imports
+import { Modal, Button } from 'antd';
+
+
+// end
 
 const { Header, Content, Footer } = Layout;
 
@@ -9,7 +15,17 @@ const DataList = () => {
     const [data,setData] = useState(null)
     const [loading,setLoading] = useState(false)
     const [error,setError] = useState(null)
-  
+
+    // modal constants
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    // end
+  // formvalues state
+
+  const [formData, setFormData]=useState()
+  const [week, setWeek] = useState()
+  const [day, setDay] = useState('')
+  const [rainData, setRainData] = useState()
+
 
     useEffect(() => {
         setLoading(true)
@@ -24,11 +40,82 @@ const DataList = () => {
             setError(error.message)
             setLoading(false)
         })
-        // let mylist = ['ann', 'luo', 'kamba', 'liz', 'alex', 'ken']
+        
       
     },[])
-  
 
+    // useEffect((formData)=>{
+     
+     
+
+      
+    // }, [])
+
+    // endpoint for posting into the backend
+
+      
+
+    // formvaulues
+//  function downStreamValues (values){
+//    setFormData(values)1
+//    console.log(formData)
+//  }
+    
+
+    // modal functionality
+    const showModal = () => {
+      setIsModalVisible(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
+  
+// useEffect((formData) =>{
+//   if (formData){
+//     postBackend(formData)
+
+//   }
+  
+// })
+//   const postBackend = (formData)=>{
+//     console.log('am notseeen')
+//     console.log(formData)
+//     if(formData){
+//       console.log('I am formdata in datalist')
+//       console.log(formData)
+//       setWeek(formData.week)
+//       setDay(formData.day)
+//       setRainData(formData.rainData)
+//     console.log('am extratctecd'+ week)
+//     fetch('http://127.0.0.1:8000/api/post/', {
+//         method:'post',
+//         cache: 'no-cache',
+//         headers: {
+//             'Content-Type': 'application/json'
+//     },
+    
+
+//         body: JSON.stringify({
+//           week: week,
+//           day: day,
+//           rainData: rainData
+//         })
+//     }).then(function(res){
+//         return res.json();
+//     })
+//     .catch(error =>{
+//         setError(error)
+//     });
+
+//     }
+    
+  
+// }
 
     return (
         <Layout>
@@ -38,14 +125,14 @@ const DataList = () => {
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
             <Menu.Item key="1">Home</Menu.Item>
             <Menu.Item key="2">Chart</Menu.Item>
-            <Menu.Item key="3">Prediction</Menu.Item>
+            <Menu.Item key="3" onClick={showModal}>Prediction</Menu.Item>
           </Menu>
         </Header>
         <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
+            <Breadcrumb.Item >App</Breadcrumb.Item>
           </Breadcrumb>
           <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
             
@@ -56,12 +143,23 @@ const DataList = () => {
                     loading = {loading}
                     data={data}
                     />
+                    
                 </div>)
         }    
             
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Rain Tracker ©2021 Created By Upande</Footer>
+
+        {/* modal jsx */}
+          <>
+            {/* <Button type="primary" onClick={showModal}>
+              Open Modal
+            </Button> */}
+            <Modal title="Data InPut" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <AntdForm upStreamValues={(values)=>setFormData(values)} />
+            </Modal>
+          </>
       </Layout>
     )
 }
